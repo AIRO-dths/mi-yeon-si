d65b4611796a32b5aec44461800b31792a35887e
imxxge <naver_vs@kakao.com>
2025-10-30 16:30:20 +0900

    api final

--- server/main.py
+++ server/main.py
@@ -14,10 +14,10 @@ os.makedirs('images', exist_ok=True)
 app = FastAPI()
 
 app.mount("/images", StaticFiles(directory="images"), name="images")
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 template = Jinja2Templates(directory="templates")
 
-users = {"ㅇㅇ_남자": 0,"ㅇㅇ_여자": 0}
 
 @app.get("/")
 def index(request: Request):
@@ -38,10 +38,10 @@ class ChatMessage(BaseModel):
 
 @app.post("/api/chat")
 async def chat_message(chat_msg: ChatMessage):
-    random_response = ''.join(random.choices(string.ascii_letters + string.digits, k=20))
+    random_response = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
     if chat_msg.counting == 5:
-        return {"response": "결과"}
-    return {"response": f"{chat_msg.user_name}님, {random_response}"}
+        return {"response": "결과 나올거 ~~~"}
+    return {"response": f"임시 대화 {random_response}"}
 
 @app.post("/api/upload_photo")
 async def upload_photo(
