d3fc3a40fac8534326bf3f75d374e788ce1818e7
imxxge <naver_vs@kakao.com>
2025-10-30 15:11:23 +0900

    rework

--- server/main.py
+++ server/main.py
@@ -1,11 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 import uvicorn
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+template = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(request: Request):
+    return template.TemplateResponse("index.html", {"request": request})
+
+@app.get("/face")
+def read_face(request: Request):
+    return template.TemplateResponse("face.html", {"request": request})
+
+@app.get("/chat")
+def read_chat(request: Request):
+    return template.TemplateResponse("chat.html", {"request": request})
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
